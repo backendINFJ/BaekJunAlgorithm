400b6aac3c4965ab9563f4e50e65bd41a0868c05
backendINFJ <163832566+backendINFJ@users.noreply.github.com>
2024-04-12 20:52:46 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        String id = a.next();
+        System.out.println(id + "??!");
+
+    }
+}
