bf15dc5b80d17bfb483761179592757f8ca01ba0
backendINFJ <163832566+backendINFJ@users.noreply.github.com>
2024-05-18 17:08:19 +0900

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
- 
-		System.out.println(A+B);
-		System.out.println(A-B);
-		System.out.println(A*B);
-		System.out.println(A/B);
-		System.out.println(A%B);
-	}
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
 }
