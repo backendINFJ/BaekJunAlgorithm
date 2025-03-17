a318b362befa74bc3bdd13f66e609195f414b36a
developer-rocket <163832566+developer-rocket@users.noreply.github.com>
2025-03-17 20:22:06 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,9 @@
 class Solution {
-   public int solution(int num1, int num2) {
-        int answer = 0;
+    public int solution(int num1, int num2) {
         if (num1 == num2) {
-         answer = 1;
-        } else if (num1 != num2) {
-            answer = -1;
+            return 1;
+        } else {
+            return -1;
         }
-        return answer;
     }
 }
