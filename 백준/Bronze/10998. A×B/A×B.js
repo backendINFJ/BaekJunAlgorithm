5209ac71d068ca5ca677f9a7c528902ce86412bd
backendINFJ <163832566+backendINFJ@users.noreply.github.com>
2025-04-04 00:16:43 +0900

    [Bronze V] Title: A×B, Time: 116 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,13 @@
+// Node.js 환경에서 실행되는 코드
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (line) => {
+    const [A, B] = line.split(' ').map(Number); // 입력받은 값을 공백으로 나누고 숫자로 변환
+    console.log(A * B); // A * B 결과 출력
+    rl.close(); // 입력 종료
+});
