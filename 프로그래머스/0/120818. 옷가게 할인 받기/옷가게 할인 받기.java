8675bb1cf047088608e503c22b2ec4cf7f29af52
backendINFJ <163832566+backendINFJ@users.noreply.github.com>
2025-04-14 23:38:57 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+
+        // 할인율 적용 조건
+        if (price >= 500000) {
+            answer = (int)(price * 0.8); // 20% 할인
+        } else if (price >= 300000) {
+            answer = (int)(price * 0.9); // 10% 할인
+        } else if (price >= 100000) {
+            answer = (int)(price * 0.95); // 5% 할인
+        } else {
+            answer = price; // 할인 없음
+        }
+
+        return answer;
+    }
+}
