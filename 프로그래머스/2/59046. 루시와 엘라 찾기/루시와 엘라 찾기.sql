9370f0c5650de69296cd9c87c9b821f01c13a8c5
infj-clover <163832566+infj-clover@users.noreply.github.com>
2026-04-02 23:48:48 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,12 @@
+/*
+1. fillter : 동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 쿼리
+2. column : ANIMAL_ID, NAME, SEX_UPON_INTAKE
+3. sort : ANIMAL_ID ASC;
+*/
+
+SELECT ANIMAL_ID
+,      NAME
+,      SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID ASC;
